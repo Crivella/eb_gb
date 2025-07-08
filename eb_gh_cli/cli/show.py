4b6ea8bddddfa5402168f87acfd916dc65590c6b
crivella <davidecrivella@yahoo.it>
2025-07-08 17:28:50 +0200

    Fix wrong param

--- eb_gh_cli/cli/show.py
+++ eb_gh_cli/cli/show.py
@@ -8,34 +8,34 @@ from .main import show
 @show.command()
 @opt.FILTER_USER_OPTION
 @click.argument('gh_repo', type=ct.GithubRepositoryType())
-def gh_repo(repo):
+def gh_repo(gh_repo):
     """Show a GitHub repository."""
-    click.echo(f'GitHub repository: {repo}')
+    click.echo(f'GitHub repository: {gh_repo}')
 
 @show.command()
 @opt.FILTER_USER_OPTION
 @opt.FILTER_REPO_OPTION
 @click.argument('gh_issue', type=ct.GithubIssueType())
-def gh_issue(issue):
+def gh_issue(gh_issue):
     """Show a GitHub issue."""
-    click.echo(f'GitHub issue: {issue}')
-    click.echo(f'Issue number: {issue.number}')
-    click.echo(f'Title: {issue.title}')
-    click.echo(f'Description: {issue.body}')
-    click.echo(f'Created by: {issue.created_by.username}')
-    click.echo(f'Closed by: {issue.closed_by.username if issue.closed_by else 'N/A'}')
+    click.echo(f'GitHub issue: {gh_issue}')
+    click.echo(f'Issue number: {gh_issue.number}')
+    click.echo(f'Title: {gh_issue.title}')
+    click.echo(f'Description: {gh_issue.body}')
+    click.echo(f'Created by: {gh_issue.created_by.username}')
+    click.echo(f'Closed by: {gh_issue.closed_by.username if gh_issue.closed_by else 'N/A'}')
     # click.echo(f'Assignee: {gh_issue.assignee.username if gh_issue.assignee else "N/A"}')
 
 @show.command()
 @opt.FILTER_USER_OPTION
 @opt.FILTER_REPO_OPTION
 @click.argument('gh_pr', type=ct.GithubPullRequestType())
-def gh_pr(pr):
+def gh_pr(gh_pr):
     """Show a Github Pull Request."""
-    click.echo(f'GitHub Pull Request: {pr}')
-    click.echo(f'Pull Request number: {pr.number}')
-    click.echo(f'Title: {pr.title}')
-    click.echo(f'Description: {pr.body}')
-    click.echo(f'Created by: {pr.created_by.username}')
+    click.echo(f'GitHub Pull Request: {gh_pr}')
+    click.echo(f'Pull Request number: {gh_pr.number}')
+    click.echo(f'Title: {gh_pr.title}')
+    click.echo(f'Description: {gh_pr.body}')
+    click.echo(f'Created by: {gh_pr.created_by.username}')
     # click.echo(f'Closed by: {gh_pr.closed_by.username if gh_pr.closed_by else "N/A"}')
-    click.echo(f'Merged by: {pr.merged_by.username if pr.merged_by else 'N/A'}')
+    click.echo(f'Merged by: {gh_pr.merged_by.username if gh_pr.merged_by else 'N/A'}')
