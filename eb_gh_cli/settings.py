d68e59b92a978a76be9d1342ebdfe26d57eb9594
crivella <davide.grassano@epfl.ch>
2025-07-11 11:31:03 +0200

    Added CAS storage with compression and cleanup unneeded settings

--- eb_gh_cli/settings.py
+++ eb_gh_cli/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+GH_DB_ROOT = os.environ.get('GH_DB_ROOT', os.path.expanduser('~/.gh_db'))
+
 
 # Logging
 LOGGING = {
@@ -57,65 +59,35 @@ LOGGING = {
     },
 }
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*uh@)iws0&=t87av(z7%znte@rh81$7av)*)jw6-sdq#g2e5m4'
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', SECRET_KEY)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-ALLOWED_HOSTS += os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(';')
-
 # Application definition
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'eb_gh_cli'
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+MIDDLEWARE = []
 
-# ROOT_URLCONF = "mysite.urls"
+TEMPLATES = []
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+STORAGES = {
+    'default': {
+        'BACKEND': 'eb_gh_cli.storage.HashStorage',
         'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+            'location': os.environ.get('STORAGE_LOCATION', os.path.join(GH_DB_ROOT, 'files')),
+            'compression': 'gzip',
+        }
     },
-]
-
-# WSGI_APPLICATION = "mysite.wsgi.application"
-
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASE_ENGINE = os.environ.get('DATABASE_ENGINE', 'django.db.backends.sqlite3')
-DATABASE_NAME = os.environ.get('DATABASE_NAME', os.path.join(BASE_DIR, 'db.sqlite3'))
+DATABASE_NAME = os.environ.get('DATABASE_NAME', None)
+if DATABASE_NAME is None:
+    os.makedirs(GH_DB_ROOT, exist_ok=True)
+    DATABASE_NAME = os.path.join(GH_DB_ROOT, 'gh_db.sqlite3')
 
 DATABASE = {
     'ENGINE': DATABASE_ENGINE,
@@ -143,26 +115,11 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+AUTH_PASSWORD_VALIDATORS = []
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -179,5 +136,4 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
